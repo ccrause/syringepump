19a6774cd4cab7d1cf915ad5315e67345bf2ee28
humanfr <humanfr@gmail.com>
2019-05-18 15:31:23 +0200

    update pin nubers to pcb

--- src/pinconfig.h
+++ src/pinconfig.h
@@ -2,17 +2,18 @@
 #define __PINCONFIG_H__
 
 //--------------------------------------------Pins--------------------------------------------------
-// #define enable 12
+#define enablePin 12 //enable= low 
 #define ms1 14 //micro stepping
 #define ms2 27 //micro stepping
 #define ms3 26 //micro stepping all on for 16th step
-#define stepPin 25
-#define dirPin 33
-#define resetPin 34 // reset stepper controller by pulling low
-#define faultPin 39 // low indicate fault on stepper controller (over temperature, over current/short)
+#define resetPin 25 // reset stepper controller by pulling low
+#define sleepPin 33
+#define stepPin 32
+#define dirPin 36
+#define faultPin 34 // low indicate fault on stepper controller (over temperature, over current/short)
 
-#define dispenseButton 35 //cycle start button
+#define dispenseButton 39 //cycle start button
 #define servo 13 // servo signal
-#define encoderPin 32 // screw encoder
+#define encoderPin 36 // screw encoder
 
 #endif // __PINCONFIG_H__
