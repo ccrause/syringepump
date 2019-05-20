6d705590601487576a6d97921df1f69eab05a3b1
ccrause <christo.crause@gmail.com>
2019-05-20 19:06:13 +0200

    dirPin is actually 35

--- src/pinconfig.h
+++ src/pinconfig.h
@@ -2,14 +2,14 @@
 #define __PINCONFIG_H__
 
 //--------------------------------------------Pins--------------------------------------------------
-#define enablePin 12 //enable= low 
+#define enablePin 12 //enable= low
 #define ms1 14 //micro stepping
 #define ms2 27 //micro stepping
 #define ms3 26 //micro stepping all on for 16th step
 #define resetPin 25 // reset stepper controller by pulling low
 #define sleepPin 33
 #define stepPin 32
-#define dirPin 36
+#define dirPin 35
 #define faultPin 34 // low indicate fault on stepper controller (over temperature, over current/short)
 
 #define dispenseButton 39 //cycle start button
