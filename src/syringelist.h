f86b860a5a0a501a057869a08068d30350cebae3
ccrause <christo.crause@gmail.com>
2020-05-16 15:30:32 +0200

    Increased drive current for larger syringes

--- src/syringelist.h
+++ src/syringelist.h
@@ -11,11 +11,11 @@ struct syringeSpec {
 };
 
 static const syringeSpec syringeInfo[] = {
-  {.vol=20 , .stroke=63.66 , .lowSpeedSGT=10, .lowSpeedCurrent=200, .highSpeedSGT=15, .highSpeedCurrent=400},
-  {.vol=50 , .stroke=109.97, .lowSpeedSGT=10, .lowSpeedCurrent=200, .highSpeedSGT=20, .highSpeedCurrent=400},
-  {.vol=100, .stroke=141.17, .lowSpeedSGT=10, .lowSpeedCurrent=200, .highSpeedSGT=20, .highSpeedCurrent=400},
-  {.vol=250, .stroke=151.61, .lowSpeedSGT=10, .lowSpeedCurrent=200, .highSpeedSGT=20, .highSpeedCurrent=400},
-  {.vol=500, .stroke=207.80, .lowSpeedSGT=10, .lowSpeedCurrent=200, .highSpeedSGT=20, .highSpeedCurrent=400}
+  {.vol=20 , .stroke=63.66 , .lowSpeedSGT=10, .lowSpeedCurrent=300, .highSpeedSGT=15, .highSpeedCurrent=400},
+  {.vol=50 , .stroke=109.97, .lowSpeedSGT=10, .lowSpeedCurrent=350, .highSpeedSGT=20, .highSpeedCurrent=450},
+  {.vol=100, .stroke=141.17, .lowSpeedSGT=10, .lowSpeedCurrent=400, .highSpeedSGT=20, .highSpeedCurrent=500},
+  {.vol=250, .stroke=151.61, .lowSpeedSGT=10, .lowSpeedCurrent=450, .highSpeedSGT=20, .highSpeedCurrent=550},
+  {.vol=500, .stroke=207.80, .lowSpeedSGT=10, .lowSpeedCurrent=500, .highSpeedSGT=20, .highSpeedCurrent=600}
 };
 
 enum syringeType {st20=0, st50, st100, st250, st500};
